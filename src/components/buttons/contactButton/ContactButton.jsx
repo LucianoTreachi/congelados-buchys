fc974f04cbd45fac7f0fd260969a6131a987b181
lucianotreachi <lucianotreachi@outlook.com>
2024-07-22 10:13:51 -0300

    Agregar efecto hover en logo y teléfono. Modificar estilos del btn de la card

--- src/components/buttons/contactButton/ContactButton.jsx
+++ src/components/buttons/contactButton/ContactButton.jsx
@@ -9,7 +9,7 @@ export default function ContactButton() {
       target="_blank"
       rel="noreferrer"
     >
-      Consultar
+      Consultar por este producto
       <svg
         className={styles.arrow}
         xmlns="http://www.w3.org/2000/svg"
