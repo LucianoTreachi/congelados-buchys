4eb65b3c6e5be1472a3a4cdea9af0d4ab5c8f52e
lucianotreachi <lucianotreachi@outlook.com>
2024-08-23 08:08:19 -0300

    Cambiar orden de productos

--- src/data/dataIndividualProducts.jsx
+++ src/data/dataIndividualProducts.jsx
@@ -3,9 +3,9 @@ export const dataIndividualProducts = [
     id: 1,
     category: "Individual",
     image:
-      "https://res.cloudinary.com/dwqk2mkgh/image/upload/f_auto,q_auto/v1699549626/congelados-buchys/individual1_nynjeo.jpg",
-    title: "Caja de 4 Hamburguesas",
-    subtitle: "(60 gramos cada hamburguesa)",
+      "https://res.cloudinary.com/dwqk2mkgh/image/upload/f_auto,q_auto/v1699550143/congelados-buchys/individual3_ps7qpl.jpg",
+    title: "Caja de 4 Hamburguesas Gourmet",
+    subtitle: "120 gramos cada hamburguesa",
     description: (
       <>
         <p>* Si llevas solo este producto la compra mínima son 10 cajas</p>
@@ -31,9 +31,9 @@ export const dataIndividualProducts = [
     id: 3,
     category: "Individual",
     image:
-      "https://res.cloudinary.com/dwqk2mkgh/image/upload/f_auto,q_auto/v1699550143/congelados-buchys/individual3_ps7qpl.jpg",
-    title: "Caja de 4 Hamburguesas Gourmet",
-    subtitle: "120 gramos cada hamburguesa",
+      "https://res.cloudinary.com/dwqk2mkgh/image/upload/f_auto,q_auto/v1699549626/congelados-buchys/individual1_nynjeo.jpg",
+    title: "Caja de 4 Hamburguesas",
+    subtitle: "(60 gramos cada hamburguesa)",
     description: (
       <>
         <p>* Si llevas solo este producto la compra mínima son 10 cajas</p>
@@ -45,9 +45,9 @@ export const dataIndividualProducts = [
     id: 4,
     category: "Individual",
     image:
-      "https://res.cloudinary.com/dwqk2mkgh/image/upload/f_auto,q_auto/v1699550411/congelados-buchys/individual4_pjkep9.jpg",
-    title: "Caja de 8 Milanesas",
-    subtitle: "1kg (125 gramos cada milanesa)",
+      "https://res.cloudinary.com/dwqk2mkgh/image/upload/f_auto,q_auto/v1699550746/congelados-buchys/individual5_qwesk0.jpg",
+    title: "Caja de 4 Hamburguesas",
+    subtitle: "(60 gramos cada hamburguesa)",
     description: (
       <>
         <p>* Si llevas solo este producto la compra mínima son 10 cajas</p>
@@ -59,9 +59,9 @@ export const dataIndividualProducts = [
     id: 5,
     category: "Individual",
     image:
-      "https://res.cloudinary.com/dwqk2mkgh/image/upload/f_auto,q_auto/v1699550746/congelados-buchys/individual5_qwesk0.jpg",
-    title: "Caja de 4 Hamburguesas",
-    subtitle: "(60 gramos cada hamburguesa)",
+      "https://res.cloudinary.com/dwqk2mkgh/image/upload/f_auto,q_auto/v1699550411/congelados-buchys/individual4_pjkep9.jpg",
+    title: "Caja de 8 Milanesas",
+    subtitle: "1kg (125 gramos cada milanesa)",
     description: (
       <>
         <p>* Si llevas solo este producto la compra mínima son 10 cajas</p>
