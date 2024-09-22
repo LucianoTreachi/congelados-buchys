677b4c35f9f49539537b483a782ec7e27e5047ba
lucianotreachi <lucianotreachi@outlook.com>
2024-09-22 17:21:14 -0300

    Modificar información

--- src/pages/home/homeSections/heroSection/HeroSection.jsx
+++ src/pages/home/homeSections/heroSection/HeroSection.jsx
@@ -6,11 +6,15 @@ export default function HeroSection() {
   return (
     <section className={styles.section}>
       <div className={styles.container}>
-        <h1 className={styles.primaryTitle}>Congelados Buchys</h1>
+        <h1 className={styles.primaryTitle}>Productos Congelados</h1>
         <h2 className={styles.secondaryTitle}>
-          Distribuidor mayorista y minorista. Ofrecemos una amplia gama de
+          Distribuidor Mayorista y Minorista. Ofrecemos una amplia gama de
           productos.
         </h2>
+        <h3 className={styles.tertiaryTitle}>
+          Hamburguesas, Lomos, Milanesas, Albóndigas, Patitas, Empanadas chinas,
+          Panificación, Papas, Postres.
+        </h3>
         <span className={styles.paragraph}>¡Envío sin cargo!</span>
       </div>
       <img
